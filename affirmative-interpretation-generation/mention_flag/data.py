6c80e14266939b455c60cdec05307098afadd627
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2023-11-08 23:29:16 -0700

    found the bug, trying to find it

--- affirmative-interpretation-generation/mention_flag/data.py
+++ affirmative-interpretation-generation/mention_flag/data.py
@@ -97,13 +97,14 @@ class AFINDataset(Dataset):
                     break
             
             if not found:
-                # mention_flag_matrix = torch.zeros((1, 50, 80))
-                # self.original_cues.append([])
-                # self.mention_flags.append(mention_flag_matrix[0])
-                not_found_index.append(i)
+                mention_flag_matrix = torch.zeros((1, 50, 80))
+                self.original_cues.append([])
+                self.mention_flags.append(mention_flag_matrix[0])
+                # not_found_index.append(i)
                 not_found += 1
                 # discarded out of the dataset 
-            if found:
+            # if found:
+            if True:
                 self.inputs.append(tokenized_inputs)
                 self.targets.append(tokenized_targets)
         # print(f"not found: {not_found}")
