2141ba355baa736392054bc640c8bc4352c0f7d6
MohammadHossein Rezaei <mhrmhr2004@gmail.com>
2023-12-20 21:11:33 -0700

    only block not

--- affirmative-interpretation-generation/mention_flag/mf_cal_2.py
+++ affirmative-interpretation-generation/mention_flag/mf_cal_2.py
@@ -235,3 +235,4 @@ print(mentionflag.shape)
 input_id = [f"{id} ({tokenizer.decode([id])})" for id in input_id[0]]
 decoder_id = [f"{id} ({tokenizer.decode([id])})" for id in decoder_id[0]]
 pretty_mf_printer(input_id, decoder_id, mentionflag[0])
+'''
